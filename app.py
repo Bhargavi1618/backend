f1363a2c843314552ebdb25edb1a042f404d32f4
Bhargavi1618 <107472225+Bhargavi1618@users.noreply.github.com>
2024-05-06 15:20:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+#CORS(app, resources={r"/receive_response": {"origins": "https://magnificent-salamander-454500.netlify.app"}})
+#CORS(app, resources={r"/*": {"origins": "*"}})  # Allow CORS for all origins
+CORS(app)
+@app.route('/receive_response', methods=['POST'])
+def receive_response():
+    print("Received a request!")
+    response_data = request.json
+    print("Response Data:", response_data)
+    # Your processing logic here
+    response = jsonify({"message": "Response received"})
+    response.headers.add('Access-Control-Allow-Origin', '*')  # Add CORS header
+    return response, 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
