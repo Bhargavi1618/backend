eff4ebafe756948ed09df730a480d7a20e15b0f2
Bhargavi1618 <107472225+Bhargavi1618@users.noreply.github.com>
2024-05-07 12:55:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 #CORS(app, resources={r"/receive_response": {"origins": "https://magnificent-salamander-454500.netlify.app"}})
